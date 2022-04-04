af49385f5c381bd3b00ad39ce2c4c051b7216a3e
van_vanich <panahno13@gmail.com>
2022-04-04 15:16:45 +0300

    Cross Site Request Forgery

--- src/main/java/com/example/study/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/study/security/ApplicationSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.example.study.security;
 
-import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,8 +12,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import static com.example.study.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.study.security.ApplicationUserRoles.ADMIN;
 import static com.example.study.security.ApplicationUserRoles.ADMINTRAINEE;
 import static com.example.study.security.ApplicationUserRoles.STUDENT;
@@ -35,14 +33,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
-//                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -55,20 +51,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails student = User.builder()
                 .username("student")
                 .password(passwordEncoder.encode("student"))
-//                .roles(STUDENT.name())   //ROLE_STUDENT
                 .authorities(STUDENT.getGrantedAuthority())
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin"))
-//                .roles(ADMIN.name())    //ROLE_ADMIN
                 .authorities(ADMIN.getGrantedAuthority())
                 .build();
         UserDetails adminTrainee = User.builder()
                 .username("admintrainee")
                 .password(passwordEncoder.encode("admintrainee"))
-//                .roles(ADMINTRAINEE.name())    //ROLE_ADMINTRAINEE
                 .authorities(ADMINTRAINEE.getGrantedAuthority())
                 .build();
 
